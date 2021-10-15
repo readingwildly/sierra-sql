760fa0b2e6cbcc45654c2484a528a42d02b05d09
readingwildly <88350965+readingwildly@users.noreply.github.com>
2021-10-15 09:19:25 -0700

    Update Holds Ready at Given Location with email address.sql

--- Holds Ready at Given Location with email address.sql
+++ Holds Ready at Given Location with email address.sql
@@ -3,5 +3,5 @@ FROM sierra_view.hold h
 JOIN sierra_view.patron_record_fullname p ON h.patron_record_id = p.patron_record_id
 JOIN sierra_view.record_metadata r ON p.patron_record_id = r.id 
 JOIN sierra_view.varfield v ON v.record_id = r.id AND record_type_code = 'p'
-WHERE (h.status = 'b' OR h.status = 'i') AND h.pickup_location_code = 'fra' 
+WHERE (h.status = 'b' OR h.status = 'i') AND h.pickup_location_code = 'xxx' 
 AND v.varfield_type_code = 'z' 
