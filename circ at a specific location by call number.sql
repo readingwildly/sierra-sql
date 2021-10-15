80406053f315586693258f3bbb554d45d29f21d7
readingwildly <88350965+readingwildly@users.noreply.github.com>
2021-10-15 09:33:58 -0700

    Update circ at a specific location by call number.sql
    Circulation for a specific location to see what types of materials patrons are checking out (e.g., mystery, science fiction, etc.), rather than relying on anecdotal reports

--- circ at a specific location by call number.sql
+++ circ at a specific location by call number.sql
@@ -1,3 +1,3 @@
-﻿SELECT i.call_number
+SELECT i.call_number
 FROM sierra_view.item_record_property i JOIN sierra_view.circ_trans c ON i.item_record_id = c.item_record_id
 WHERE c.op_code = 'o' AND (c.stat_group_code_num = '99' OR c.stat_group_code_num = '999')
